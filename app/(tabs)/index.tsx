177f74347b14faae401f981a15b0955ec55453e3
DulranSam <dulransamarasinghe4@gmail.com>
2024-07-05 17:09:01 +0530

    minor changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,21 @@
-import { Link } from 'expo-router';
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { styles } from '../../styles';
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  const handleGetStarted = () => {
+    navigation.navigate('user-type'); // Navigate to UserTypeScreen
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hi , Welcome to Kemi 👋🏻</Text>
-      <Link href="/user-type" asChild>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Get Started</Text>
-        </TouchableOpacity>
-      </Link>
+      <Text style={styles.title}>Hi, Welcome to Kemi 👋🏻</Text>
+      <TouchableOpacity style={styles.button} onPress={handleGetStarted}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 }
